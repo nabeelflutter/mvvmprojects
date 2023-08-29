852bef78a51269875b29851e1ff35033ec453132
sajjadalijaved <sajjadali11755@gmail.com>
2023-08-29 23:23:18 +0500

    save

--- lib/data/app_exception.dart
+++ lib/data/app_exception.dart
@@ -1,31 +1,28 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 
 class AppException implements Exception {
+  // ignore: unused_field
   final _message;
+  // ignore: unused_field
   final _prefix;
 
   AppException([this._message, this._prefix]);
 }
 
 class FetchDataException extends AppException {
-
   FetchDataException([String? message])
       : super(message, 'Error During Communication');
 }
 
 class BadRequestException extends AppException {
-
-  BadRequestException([String? message])
-      : super(message, 'Invalid request');
+  BadRequestException([String? message]) : super(message, 'Invalid request');
 }
 
 class UnAuthorizedException extends AppException {
-
   UnAuthorizedException([String? message])
       : super(message, 'UnAuthorized request');
 }
-class InvalidInputException extends AppException {
 
-  InvalidInputException([String? message])
-      : super(message, 'Invalid Input');
+class InvalidInputException extends AppException {
+  InvalidInputException([String? message]) : super(message, 'Invalid Input');
 }
